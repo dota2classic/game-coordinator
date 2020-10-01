a2e8717e5c5c8bac0039458c4b090aa1e71cd3be
itachi <enchantinggg4@gmail.com>
2020-10-01 22:24:42 +0300

    Fixed build

--- src/mm/room/model/entity/room-size.ts
+++ src/mm/room/model/entity/room-size.ts
@@ -2,5 +2,6 @@ import { MatchmakingMode } from "src/mm/queue/model/entity/matchmaking-mode";
 
 export const RoomSizes: { [key in MatchmakingMode]: number } = {
   [MatchmakingMode.SOLOMID]: 2,
-  [MatchmakingMode.RANKED]: 10
+  [MatchmakingMode.RANKED]: 10,
+  [MatchmakingMode.UNRANKED]: 10
 };
