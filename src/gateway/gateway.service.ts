db5b57bdd7297fc549490bcc614e3a98fbeefbaf
itachi <enchantinggg4@gmail.com>
2020-10-02 01:53:24 +0300

    Refactored gateway events and added queue-created event

--- src/gateway/gateway.service.ts
+++ src/gateway/gateway.service.ts
@@ -2,12 +2,17 @@ import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { Client, ClientProxy, Transport } from "@nestjs/microservices";
 import { EventBus, ofType } from "@nestjs/cqrs";
 import { QueueUpdateEvent } from "src/mm/queue/event/queue-update.event";
-import { observeOn, tap } from "rxjs/operators";
-import { GatewayQueueEntry, GatewayQueueUpdatedEvent } from "src/gateway/gateway/events/gateway-queue-updated.event";
-import { asyncScheduler } from "rxjs";
+import { map, observeOn } from "rxjs/operators";
+import {
+  GatewayQueueEntry,
+  GatewayQueueUpdatedEvent,
+} from "src/gateway/gateway/events/gateway-queue-updated.event";
+import { asyncScheduler, merge, Observable } from "rxjs";
 import { PartyRepository } from "src/mm/party/repository/party.repository";
 import { QueueRepository } from "src/mm/queue/repository/queue.repository";
 import { asyncMap } from "rxjs-async-map";
+import { QueueCreatedEvent } from "src/mm/queue/event/queue-created.event";
+import { GatewayQueueCreatedEvent } from "src/gateway/gateway/events/gateway-queue-created.event";
 
 @Injectable()
 export class GatewayService implements OnApplicationBootstrap {
@@ -20,29 +25,41 @@ export class GatewayService implements OnApplicationBootstrap {
   @Client({ transport: Transport.TCP, options: { port: 5001 } })
   private readonly discordGateway: ClientProxy;
 
+  protected queueUpdated(): Observable<GatewayQueueUpdatedEvent> {
+    return this.ebus.pipe(
+      observeOn(asyncScheduler),
+      ofType(QueueUpdateEvent),
+      asyncMap(async t => {
+        const q = await this.queueRepository.get(t.mode);
+        return new GatewayQueueUpdatedEvent(
+          t.mode,
+          q.entries.map(
+            t =>
+              new GatewayQueueEntry(
+                t.partyID,
+                t.players.map(it => it.playerId),
+              ),
+          ),
+        );
+      }, 2),
+    );
+  }
+
+  protected queueCreated(): Observable<GatewayQueueCreatedEvent> {
+    return this.ebus.pipe(
+      observeOn(asyncScheduler),
+      ofType(QueueCreatedEvent),
+      map(e => new GatewayQueueCreatedEvent(e.mode)),
+    );
+  }
+
   async onApplicationBootstrap() {
     try {
       await this.discordGateway.connect();
     } catch (e) {}
-    this.ebus
-      .pipe(
-        observeOn(asyncScheduler),
-        ofType(QueueUpdateEvent),
-        asyncMap(async t => {
-          const q = await this.queueRepository.get(t.mode);
-          return new GatewayQueueUpdatedEvent(
-            t.mode,
-            q.entries.map(
-              t =>
-                new GatewayQueueEntry(
-                  t.partyID,
-                  t.players.map(it => it.playerId),
-                ),
-            ),
-          );
-        }, 2),
-        tap(t => this.discordGateway.emit(t.constructor.name, t)),
-      )
-      .subscribe();
+
+    const mappers = [this.queueUpdated(), this.queueCreated()];
+
+    merge(...mappers).subscribe(t => this.discordGateway.emit(t.constructor.name, t));
   }
 }
