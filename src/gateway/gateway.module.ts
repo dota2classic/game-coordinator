9422f755551a5f384f9b36c30e7fb984162257a6
itachi <enchantinggg4@gmail.com>
2020-11-01 22:24:32 +0300

    Ok fixed redis queue

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -3,7 +3,7 @@ import { GatewayService } from "src/gateway/gateway.service";
 import { CqrsModule } from "@nestjs/cqrs";
 import { MmModule } from "src/mm/mm.module";
 import { ClientsModule, Transport } from "@nestjs/microservices";
-import { REDIS_URL } from "src/@shared/env";
+import {REDIS_PASSWORD, REDIS_URL} from "src/@shared/env";
 import { QueryController } from "src/gateway/query.controller";
 import { CommandController } from "src/gateway/command.controller";
 import { GetPlayerInfoQuery } from "src/gateway/gateway/queries/GetPlayerInfo/get-player-info.query";
@@ -19,6 +19,9 @@ import { outerQuery } from "src/gateway/gateway/util/outerQuery";
         transport: Transport.REDIS,
         options: {
           url: REDIS_URL(),
+          password: REDIS_PASSWORD(),
+          retryAttempts: Infinity,
+          retryDelay: 3000
         },
       },
     ]),
