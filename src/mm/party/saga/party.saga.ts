d4cfe895ea4d13d1450f96bdc836970fb43a69a6
itachi <enchantinggg4@gmail.com>
2020-11-10 13:40:11 +0300

    Fixed build

--- src/mm/party/saga/party.saga.ts
+++ src/mm/party/saga/party.saga.ts
@@ -1,12 +1,12 @@
-import {Injectable} from "@nestjs/common";
-import {ICommand, ofType, Saga} from "@nestjs/cqrs";
-import {Observable} from "rxjs";
-import {delay, map} from "rxjs/operators";
-import {PartyInviteAcceptedEvent} from "src/gateway/gateway/events/party-invite-accepted.event";
-import {AcceptPartyInviteCommand} from "src/mm/party/command/AcceptPartyInvite/accept-party-invite.command";
-import {PartyInviteCreatedEvent} from "src/gateway/gateway/events/party-invite-created.event";
-import {PARTY_INVITE_LIFETIME} from "src/gateway/gateway/shared-types/timings";
-import {TimeoutPartyInviteCommand} from "src/mm/party/command/TimeoutPartyInvite/timeout-party-invite.command";
+import { Injectable } from "@nestjs/common";
+import { ICommand, ofType, Saga } from "@nestjs/cqrs";
+import { Observable } from "rxjs";
+import { delay, map } from "rxjs/operators";
+import { PartyInviteAcceptedEvent } from "src/gateway/gateway/events/party-invite-accepted.event";
+import { AcceptPartyInviteCommand } from "src/mm/party/command/AcceptPartyInvite/accept-party-invite.command";
+import { PartyInviteCreatedEvent } from "src/gateway/gateway/events/party-invite-created.event";
+import { PARTY_INVITE_LIFETIME } from "src/gateway/gateway/shared-types/timings";
+import { TimeoutPartyInviteCommand } from "src/mm/party/command/TimeoutPartyInvite/timeout-party-invite.command";
 
 @Injectable()
 export class PartySaga {
@@ -14,7 +14,7 @@ export class PartySaga {
   acceptInvite = (events$: Observable<any>): Observable<ICommand> => {
     return events$.pipe(
       ofType(PartyInviteAcceptedEvent),
-      map(e => new AcceptPartyInviteCommand(e.inviteId)),
+      map(e => new AcceptPartyInviteCommand(e.inviteId, e.accept)),
     );
   };
 
