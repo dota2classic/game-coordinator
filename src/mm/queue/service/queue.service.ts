2e00ebf131656de7c36e03f1582eea057d8a247d
itachi <enchantinggg4@gmail.com>
2020-11-16 01:54:11 +0300

    Removed obsolete todo

--- src/mm/queue/service/queue.service.ts
+++ src/mm/queue/service/queue.service.ts
@@ -17,7 +17,6 @@ export class QueueService {
   private findRankedGame(q: QueueModel): QueueGameEntity | undefined {
     if (q.size < RoomSizes[q.mode]) return undefined;
 
-    // todo
     return QueueService.balancedGameSearch(q);
   }
 
