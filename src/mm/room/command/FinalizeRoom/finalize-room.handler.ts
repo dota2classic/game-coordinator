bbb4e843ec3f5c5f3183ba21d2fb784851dd03cb
Yuri Stepin <enchantinggg4@gmail.com>
2024-09-21 03:14:38 +0300

    Fix

--- src/mm/room/command/FinalizeRoom/finalize-room.handler.ts
+++ src/mm/room/command/FinalizeRoom/finalize-room.handler.ts
@@ -1,9 +1,13 @@
 import { CommandHandler, EventBus, ICommandHandler } from "@nestjs/cqrs";
 import { Logger } from "@nestjs/common";
 import { FinalizeRoomCommand } from "mm/room/command/FinalizeRoom/finalize-room.command";
-import { RoomReadyEvent } from "gateway/gateway/events/room-ready.event";
+import {
+  MatchPlayer,
+  RoomReadyEvent,
+} from "gateway/gateway/events/room-ready.event";
 import { RoomRepository } from "mm/room/repository/room.repository";
 import { RoomNotReadyEvent } from "gateway/gateway/events/room-not-ready.event";
+import { DotaTeam } from "../../../../gateway/gateway/shared-types/dota-team";
 
 @CommandHandler(FinalizeRoomCommand)
 export class FinalizeRoomHandler
@@ -21,18 +25,25 @@ export class FinalizeRoomHandler
     // ok here we basically publish event of room-ready
 
     if (command.state.accepted === command.state.total) {
-      const radiant = room.balance.teams[0].parties.flatMap(t =>
-        t.players.map(t => t.playerId),
+      const radiant: MatchPlayer[] = room.balance.teams[0].parties.flatMap(t =>
+        t.players.map(t => ({
+          playerId: t.playerId,
+          team: DotaTeam.RADIANT,
+          name: "",
+        })),
       );
       const dire = room.balance.teams[1].parties.flatMap(t =>
-        t.players.map(t => t.playerId),
+        t.players.map(t => ({
+          playerId: t.playerId,
+          team: DotaTeam.DIRE,
+          name: "",
+        })),
       );
       this.ebus.publish(
         new RoomReadyEvent(
           command.roomId,
           command.mode,
-          radiant,
-          dire,
+          [...radiant, ...dire],
           room.balance.averageMMR,
           room.version,
         ),
