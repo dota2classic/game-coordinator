4da77f039dfd61e5f512d9ff7328b54c274771de
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-21 19:03:42 +0300

    Cleanup

--- src/mm/queue/service/balance.service.ts
+++ src/mm/queue/service/balance.service.ts
@@ -10,13 +10,6 @@ import {
 
 @Injectable()
 export class BalanceService {
-  private static readonly RECENT_WINRATE_CAP: number = 20;
-  private static readonly WINRATE_FACTOR: number = 1500;
-  private static readonly MAX_AVG_DIFF: number = 100;
-  private static readonly MAX_SCORE_DIFFERENCE: number = 500;
-  private static readonly MAX_RATING_DIFFERENCE: number = 1000;
-  private static readonly DEVIATION_MAX_FACTOR = 500;
-  private static readonly DEVIATION_MAX_SCORE = 10;
 
   private static getPartyFactor(count: number): number {
     // keep score same for single players and higher for parties
@@ -71,14 +64,6 @@ export class BalanceService {
     );
 
     return scoreSum * BalanceService.getPartyFactor(players.length);
-  }
-
-  private static calculateScoreDeviation(dScore: number) {
-    return (
-      (Math.min(BalanceService.DEVIATION_MAX_SCORE, dScore) /
-        BalanceService.DEVIATION_MAX_SCORE) *
-      BalanceService.DEVIATION_MAX_FACTOR
-    );
   }
 
   public static soloMidBalance(teamSize: number, entries: QueueEntryModel[]) {
@@ -137,114 +122,5 @@ export class BalanceService {
       throw new BalanceException("Can't even balance bot game hah");
 
     return new RoomBalance([new TeamEntry(r, 0), new TeamEntry(d, 0)]);
-  }
-
-  public static rankedBalance(
-    teamSize: number,
-    parties: QueueEntryModel[],
-    mmrDiffStrict = true,
-  ): RoomBalance {
-    let radiantScore = 0;
-    let direScore = 0;
-
-    const radiantParties: QueueEntryModel[] = [];
-    const direParties: QueueEntryModel[] = [];
-
-    let radiantPlayerCount = 0;
-    let direPlayerCount = 0;
-
-    const preparedParties = parties.sort((a, b) => b.score - a.score);
-
-    preparedParties.forEach((it, idx) => {
-      if (
-        // if radiant less mmr and
-        (radiantScore <= direScore && radiantPlayerCount < teamSize) ||
-        direPlayerCount === teamSize
-      ) {
-        radiantParties.push(it);
-        radiantPlayerCount += it.players.length;
-        radiantScore += it.score;
-      } else if (
-        (direScore <= radiantScore && direPlayerCount < teamSize) ||
-        radiantPlayerCount === teamSize
-      ) {
-        direParties.push(it);
-        direPlayerCount += it.players.length;
-        direScore += it.score;
-      } else if (radiantPlayerCount < teamSize) {
-        radiantParties.push(it);
-        radiantPlayerCount += it.players.length;
-        radiantScore += it.score;
-      } else if (direPlayerCount < teamSize) {
-        direParties.push(it);
-        direPlayerCount += it.players.length;
-        direScore += it.score;
-      }
-    });
-
-    if (radiantPlayerCount !== teamSize || direPlayerCount !== teamSize) {
-      throw new BalanceException(
-        `Final size dont fit ${radiantPlayerCount} ${direPlayerCount}`,
-      );
-    }
-
-    const rAvrg = radiantScore / teamSize;
-    const dAvrg = direScore / teamSize;
-
-    if (
-      mmrDiffStrict &&
-      Math.abs(rAvrg - dAvrg) >= BalanceService.MAX_AVG_DIFF
-    ) {
-      throw new BalanceException(
-        `Radiant ${rAvrg} Dire ${dAvrg}. Diff: ${Math.abs(
-          rAvrg - dAvrg,
-        )}, limit: ${BalanceService.MAX_AVG_DIFF}`,
-      );
-    }
-
-    return new RoomBalance(
-      [radiantParties, direParties].map(
-        (list) =>
-          new TeamEntry(
-            list,
-            list.reduce((a, b) => a + b.score, 0),
-          ),
-      ),
-    );
-  }
-
-  public static fillBalance(teamSize: number, parties: QueueEntryModel[]) {
-    const radiantParties: QueueEntryModel[] = [];
-    const direParties: QueueEntryModel[] = [];
-
-    let radiantPlayerCount = 0;
-    let direPlayerCount = 0;
-
-    parties.forEach((party) => {
-      if (radiantPlayerCount < teamSize) {
-        radiantParties.push(party);
-        radiantPlayerCount += party.size;
-      } else {
-        direParties.push(party);
-        direPlayerCount += party.size;
-      }
-    });
-
-
-    if (radiantPlayerCount !== teamSize || direPlayerCount !== teamSize) {
-      throw new BalanceException(
-        `Final size dont fit ${radiantPlayerCount} ${direPlayerCount}`,
-      );
-    }
-
-    return new RoomBalance(
-      [radiantParties, direParties].map(
-        (list) =>
-          new TeamEntry(
-            list,
-            list.reduce((a, b) => a + b.score, 0),
-          ),
-      ),
-    );
   }
 }
